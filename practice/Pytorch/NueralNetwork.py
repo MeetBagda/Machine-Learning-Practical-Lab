5b35ed6193959cdeb6438782cdc09826d5b8a94c
MeetBagda <bagdameet@gmail.com>
2025-07-06 20:14:22 +0530

    Add Simple Neural Network implementation for XOR problem using PyTorch

---
+++ practice/Pytorch/NueralNetwork.py
@@ -0,0 +1,49 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+# Sample data: 4 samples, 2 features each
+X = torch.tensor([[0., 0.],
+                  [0., 1.],
+                  [1., 0.],
+                  [1., 1.]])
+
+# Labels: binary output
+y = torch.tensor([[0.],
+                  [1.],
+                  [1.],
+                  [0.]])  # XOR Problem
+
+class SimpleNN(nn.Module):
+    def __init__(self) -> None:
+        super(SimpleNN, self).__init__()
+        self.forwardconnection1 = nn.Linear(2,4)
+        self.forwardconnection2 = nn.Linear(4,1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        x = torch.relu(self.forwardconnection1(x))
+        x = self.sigmoid(self.forwardconnection2(x))
+        return x
+    
+
+model = SimpleNN()
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters(), lr=0.01)
+
+epochs = 1000
+for epoch in range(epochs):
+    optimizer.zero_grad()
+    output = model(X)
+    loss = criterion(output, y)
+    loss.backward()
+    optimizer.step()
+
+    if(epoch+1) % 100 == 0:
+        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+
+with torch.no_grad():
+    preds = model(X)
+    predicted_classes = preds.round()
+    print(predicted_classes)
