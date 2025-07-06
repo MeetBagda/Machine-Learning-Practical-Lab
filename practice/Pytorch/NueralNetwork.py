da190a4a9507e0a5b7423b309ab2f48c70237284
MeetBagda <bagdameet@gmail.com>
2025-07-06 20:26:55 +0530

    Add comments to enhance understanding of the SimpleNN implementation and training process

--- practice/Pytorch/NueralNetwork.py
+++ practice/Pytorch/NueralNetwork.py
@@ -19,7 +19,9 @@ class SimpleNN(nn.Module):
         super(SimpleNN, self).__init__()
         self.forwardconnection1 = nn.Linear(2,4)
         self.forwardconnection2 = nn.Linear(4,1)
-        self.sigmoid = nn.Sigmoid()
+        self.sigmoid = nn.Sigmoid() # This line initializes the Sigmoid activation function.
+        # The Sigmoid function squashes the output to a value between 0 and 1.
+        # It’s perfect for binary classification problems because it outputs probabilities.
 
     def forward(self, x):
         x = torch.relu(self.forwardconnection1(x))
@@ -33,17 +35,18 @@ optimizer = optim.Adam(model.parameters(), lr=0.01)
 
 epochs = 1000
 for epoch in range(epochs):
-    optimizer.zero_grad()
+    optimizer.zero_grad() #It clears the previous gradients before the new backward pass.
     output = model(X)
     loss = criterion(output, y)
-    loss.backward()
-    optimizer.step()
+    loss.backward() # It computes gradients of the loss w.r.t all the parameters (weights & biases) using backpropagation.
+    optimizer.step() # Updates the model parameters (weights and biases) using the computed gradients.
+    # This is where learning happens — the optimizer (like Adam) adjusts the weights so that the next prediction should result in lower loss.
 
     if(epoch+1) % 100 == 0:
         print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
 
 
-with torch.no_grad():
+with torch.no_grad(): # It tells PyTorch not to track gradients during inference. We don’t need gradients while predicting (only needed for training)
     preds = model(X)
-    predicted_classes = preds.round()
+    predicted_classes = preds.round() # It rounds the output probabilities to either 0 or 1.
     print(predicted_classes)
