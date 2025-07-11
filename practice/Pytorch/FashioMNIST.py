80c688bd756c0a4193c017f05849b4ea6598315d
MeetBagda <bagdameet@gmail.com>
2025-07-11 22:23:36 +0530

    Add initial implementation for Fashion-MNIST dataset loading and preprocessing

---
+++ practice/Pytorch/FashioMNIST.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+import torchvision.datasets
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# load the Fashion-MNIST
+print("Loading dataset")
+fashionmnist = torchvision.datasets.FashionMNIST(train=True)
+x=fashionmnist.data
+y=fashionmnist.targets
+print(f"Features shape : {x.shape}")
+print(f"Label shape : {y.shape}")
+print(f"Target Classes : {fashionmnist.targets}")
+
