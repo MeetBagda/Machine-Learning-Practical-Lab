651c7121bd45c4fe814291af613c89ca347137f0
MeetBagda <bagdameet@gmail.com>
2025-07-17 12:43:33 +0530

    feat: Implement forward pass method for Perceptron class with input validation and activation output

--- DL-FROM-SCRATCH/nueron.py
+++ DL-FROM-SCRATCH/nueron.py
@@ -43,4 +43,29 @@ class Perceptron:
         print(f"Initial weights : {self.weights}")
         print(f"Initial bias : {self.bias}")
 
-    
+    def forward(self, inputs):
+        """
+        Performs the forward pass of the perceptron.
+
+        ARGS: 
+            iputs (np.array): a 1D numpy array of input values, 
+                matching the number of inputs defiend. 
+
+        Return: 
+            float: The activated output of the perceptron.
+        """
+        if(len(inputs) != len(self.weights)):
+            raise ValueError(f"Expected {len(self.weights)} inputs, but got {len(inputs)}")
+        
+        # step-1 : calculate the weighted sum of inputs and add the bias
+        weighted_sum = np.dot(inputs, self.weights) + self.bias
+
+        # step-2 : apply the activation function to the weighted sum
+        output = self.activation_fn(weighted_sum)
+
+        print(f"Input: {inputs}")
+        print(f"Weighted Sum (before activation): {weighted_sum:.4f}")
+        print(f"Output (after actibation): {output:.4f}")
+        return output
+
+
