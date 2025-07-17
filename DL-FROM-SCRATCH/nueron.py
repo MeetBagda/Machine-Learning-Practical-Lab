bc7babfdbea85b7fe088fee3f57da1db314cad3a
MeetBagda <bagdameet@gmail.com>
2025-07-17 11:51:06 +0530

    feat: Add Perceptron class with initialization and activation function support

--- DL-FROM-SCRATCH/nueron.py
+++ DL-FROM-SCRATCH/nueron.py
@@ -18,3 +18,29 @@ def relu(x):
     Outputs x if x > 0, else outputs 0.
     """
     return np.maximum(0, x)
+
+
+class Perceptron: 
+    def __init__(self, num_inputs, activation_fn=sigmoid):
+        """
+        Initialize the single perceptron/nueron.
+
+        ARGS: 
+            num_inputs (int): the number of input features this nueron expects.
+            activation_fn: the activation function to use.
+        """
+        # weights: one weight for each input
+        # Initialize with small random values to break symmetry and avoid dead nuerons (for relu)
+        self.weights = np.random.randn(num_inputs) * 0.01
+
+        # bias : a single value that shifts the activation function output.
+        # initialize with small random value
+        self.bias = np.random.randn() * 0.01
+
+        self.activation_fn = activation_fn
+
+        print(f"Perceptron initialized with {num_inputs} inputs.")
+        print(f"Initial weights : {self.weights}")
+        print(f"Initial bias : {self.bias}")
+
+    
